42f54156d830948a9350f59e0063fa477bf4b90e
splatEric <mike.smith@camc-ltd.co.uk>
2013-08-01 08:46:41 +0100

    + [OE-3272] ensure event actions displayed in view mode

--- views/default/view.php
+++ views/default/view.php
@@ -1,6 +1,9 @@
 <?php 	$this->breadcrumbs=array($this->module->id);
 	$this->header();
 ?>
+
+<?php  $this->renderPartial('//patient/event_actions'); ?>
+
 <h3 class="withEventIcon" style="background:transparent url(<?php echo $this->assetPath?>/img/medium.png) center left no-repeat;"><?php  echo $this->event_type->name ?></h3>
 
 <div>
