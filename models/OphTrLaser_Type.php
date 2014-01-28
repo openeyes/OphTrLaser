2da7250db5dfe6ffa04cc7e855c7ba07b74db612
Mark <m@rkw.me>
2014-01-28 10:44:13 +0000

    [OE-3856] table versioning bugfixes

--- models/OphTrLaser_Type.php
+++ models/OphTrLaser_Type.php
@@ -24,7 +24,7 @@
  * @property string $name
  */
 
-class OphTrLaser_Type extends BaseActiveRecord
+class OphTrLaser_Type extends BaseActiveRecordVersioned
 {
 	/**
 	 * Returns the static model of the specified AR class.
